8428d955717020533213d6d9520d136effe55cb7
ranjan1995 <ranjan&GIT9635>
2019-05-25 02:55:40 +0530

    webview Done With Image

--- app/src/main/java/com/example/tutorial/JavaActivity.java
+++ app/src/main/java/com/example/tutorial/JavaActivity.java
@@ -3,18 +3,32 @@ package com.example.tutorial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class JavaActivity extends AppCompatActivity {
     private TextView content;
     private String html;
+    WebView browser;
 
-    @Override
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
         content = findViewById(R.id.home);
         html = getIntent().getStringExtra("html");
         content.setText(Html.fromHtml(html));
+    }*/
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_java);
+        browser=(WebView)findViewById(R.id.webView);
+        html = getIntent().getStringExtra("html");
+
+        browser.loadDataWithBaseURL("file:///android_asset/",html,
+                "text/html", "UTF-8",null);
     }
+
 }
