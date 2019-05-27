b005dcd9be299a73e421813cf26062905899176d
sharmi <mondira.p@encoders.co.in>
2019-05-27 11:57:00 +0530

    Redesign toolbar

--- app/src/main/java/com/example/tutorial/JavaActivity.java
+++ app/src/main/java/com/example/tutorial/JavaActivity.java
@@ -3,14 +3,18 @@ package com.example.tutorial;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class JavaActivity extends AppCompatActivity {
     private TextView content;
     private String html;
     private String flag;
-    WebView browser;
+    private WebView browser;
+    private ImageView backButton;
+    private TextView title;
 
 
     /*@Override
@@ -26,17 +30,26 @@ public class JavaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
         browser= findViewById(R.id.webView);
+        backButton= findViewById(R.id.backButton);
+        title= findViewById(R.id.title);
         html = getIntent().getStringExtra("html");
         flag = getIntent().getStringExtra("flag");
         browser.loadDataWithBaseURL("file:///android_asset/",html,
                 "text/html", "UTF-8",null);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         if (flag != null) {
-            myToolbar.setTitle(flag);
+            title.setText(flag);
         } else {
-            myToolbar.setTitle(R.string.app_name);
+            title.setText(R.string.app_name);
         }
         setSupportActionBar(myToolbar);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 
 }
